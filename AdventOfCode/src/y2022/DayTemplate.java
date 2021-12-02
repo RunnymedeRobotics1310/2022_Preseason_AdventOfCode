c33229d9596546ce5b8a683316d861748a6d0ade
Richard McMullin <richard.mcmullin@altairix.com>
2021-12-02 07:53:23 -0500

    Set up project

---
+++ AdventOfCode/src/y2022/DayTemplate.java
@@ -0,0 +1,30 @@
+package y2022;
+
+public class DayTemplate extends AdventOfCodeSolver {
+
+	public static void main(String[] args) {
+		debug = false;
+		new DayTemplate().run();
+	}
+
+	@Override
+	protected void parseInput() {
+
+		for (String inputLine: inputLines) {
+
+			// Turn the input into something useful
+		}
+	}
+
+	@Override
+	protected void solvePart1() {
+
+		// Do something and print the answer
+	}
+
+	@Override
+	protected void solvePart2() {
+
+		// Do something and print the answer
+	}
+}
---
+++ AdventOfCode/src/y2022/day01/Day01.java
@@ -0,0 +1,32 @@
+package y2022.day01;
+
+import y2022.AdventOfCodeSolver;
+
+public class Day01 extends AdventOfCodeSolver {
+
+	public static void main(String[] args) {
+		debug = true;
+		new Day01().run();
+	}
+
+	@Override
+	protected void parseInput() {
+
+		for (String inputLine: inputLines) {
+
+			// Turn the input into something useful
+		}
+	}
+
+	@Override
+	protected void solvePart1() {
+
+		// Do something and print the answer
+	}
+
+	@Override
+	protected void solvePart2() {
+
+		// Do something and print the answer
+	}
+}
